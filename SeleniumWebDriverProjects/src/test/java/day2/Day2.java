cc034305e57e863308ae7b692c5fdfa2d5c20eeb
sagarv8090 <sagarvishwakarma08032000@gmai.com>
2025-01-17 09:30:15 +0530

    day2 working

--- SeleniumWebDriverProjects/src/test/java/day2/Day2.java
+++ SeleniumWebDriverProjects/src/test/java/day2/Day2.java
@@ -1,10 +1,51 @@
 package day2;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        // Initialize the ChromeDriver to open Chrome browser
+        WebDriver driver = new ChromeDriver();
+        
+        // Navigate to the specified URL (in this case, Zomato website)
+        driver.get("https://zomato.com/");  // This URL can be changed to any other website
+        driver.manage().window().maximize();  // Maximize the browser window for better visibility
+        
+        // ----------------- Example 1: Using 'name' attribute -----------------
+        // This can be used to locate elements with the 'name' attribute (e.g., search box)
+        // driver.findElement(By.name("search")).sendKeys("Sagar");  // Sends "Sagar" to the search box
+        
+        // ----------------- Example 2: Using 'id' attribute -----------------
+        // This can be used to locate elements with the 'id' attribute (e.g., logo on the page)
+        // Boolean isLogoDisplay = driver.findElement(By.id("logo")).isDisplayed();  // Checks if logo is displayed
+        // System.out.println(isLogoDisplay);  // Prints whether the logo is displayed (true/false)
+        
+        // ----------------- Example 3: Using 'linkText' and 'partialLinkText' -----------------
+        // 'linkText' is used for full link text matching
+        // driver.findElement(By.linkText("Tablets")).click();  // Clicks on the full link text "Tablets"
+        
+        // 'partialLinkText' is used for partial link text matching
+        // driver.findElement(By.linkText("Table")).click();  // Clicks on the partial link text "Table" (not preferred)
+        
+        // ----------------- Example 4: Using 'className' attribute -----------------
+        // 'className' is used to locate elements by their 'class' attribute
+        // In this case, we're finding all header links on the page
+        // List<WebElement> headerLinks = driver.findElements(By.className("list-inline-item"));
+        // System.out.println("Total number of header links: " + headerLinks.size());  // Prints the number of header links
+        
+        // ----------------- Example 5: Using 'tagName' to find 'a' (anchor) tags -----------------
+        // This will find all anchor tags (<a>) on the page (i.e., links)
+        // List<WebElement> links = driver.findElements(By.tagName("a"));
+        // System.out.println("Total number of links: " + links.size());  // Prints the total number of links on the page
+        
+        // ----------------- Example 6: Using 'tagName' to find 'img' tags -----------------
+        // This will find all image tags (<img>) on the page
+        // List<WebElement> images = driver.findElements(By.tagName("img"));
+        // System.out.println("Total number of images: " + images.size());  // Prints the total number of images on the page
+        
+        // Closing the browser after all operations are done
+        driver.quit();  // Always good practice to quit the driver after test completion to free up resources
+    }
 }
--- SeleniumWebDriverProjects/src/test/java/day3/Day3.java
+++ SeleniumWebDriverProjects/src/test/java/day3/Day3.java
@@ -1,10 +1,34 @@
 package day3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+		
+		//tag and id combination (tag#id)->tag is optional here (input in this case)
+//		driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("sagar");
+//		driver.findElement(By.cssSelector("#small-searchterms")).sendKeys("sagar");
+		
+		//tag and class combination(tag.classname)->tag is optional here
+//		driver.findElement(By.cssSelector("input.search-box-text")).sendKeys("T-shirts");
+//		driver.findElement(By.cssSelector(".search-box-text")).sendKeys("T-shirts");//can be used for selection multiple webelements
+		
+		// Tag with attribute (tag[attribute="value"])
+		driver.findElement(By.cssSelector("input[placeholder=\"Search store\"]")).sendKeys("T-shirts");
+		driver.findElement(By.cssSelector("[placeholder=\"Search store\"]")).sendKeys("T-shirts");
+		
+		//tag class attribute(tag.classname[attribute='x'])->tag can be removed
+//		driver.findElement(By.cssSelector("input.search-box-text[name='q']")).sendKeys("Sagar Vishwakarma");
+//		driver.findElement(By.cssSelector(".search-box-text[name='q']")).sendKeys("Sagar Vishwakarma");
+		
 
+		
 	}
 
 }
