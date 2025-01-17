102b0475b22310ff6a928cea7e554770b10a1159
sagarv8090 <sagarvishwakarma08032000@gmai.com>
2025-01-17 14:42:23 +0530

    upadating xpath learning

--- SeleniumWebDriverProjects/src/test/java/day4/Day4.java
+++ SeleniumWebDriverProjects/src/test/java/day4/Day4.java
@@ -6,39 +6,51 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Day4 {
 
-	public static void main(String[] args) {
-		
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://demo.nopcommerce.com/");
-		driver.manage().window().maximize();
-		
-		//Xpath with single attribute: -> //tagname[@attribute=”value”]
-		//driver.findElement(By.xpath("//input[@placeholder='Search store']")).sendKeys("sagar");
-		
-		//Xpath with multiple attribute: -> //tagname[@attribute1=’value1’][@attribute2=’value2’]
-		//driver.findElement(By.xpath("//input[@placeholder='Search store'][@id='small-searchterms']")).sendKeys("sagar vishwakarma");
-		
-		//Xpath with ‘and’ operator: ->tagname[@name=’search’ and @placeholder=’search here’]
-		//driver.findElement(By.xpath("//input[@placeholder='Search store' and @id='small-searchterms']")).sendKeys("sagar vishwakarma");
-		
-		//Xpath with ‘or operator: ->tagname[@name=’search’ or @placeholder=’search here’]
-		//driver.findElement(By.xpath("//input[@placeholder='Search store' or @id='small-searcrms']")).sendKeys("sagar vishwakarma");
-		
-		//Xpath with text(): -> inner text
-		//Boolean isDisplay=driver.findElement(By.xpath("//h1[text()='Computers']")).isDisplayed();
-		//Boolean isDisplay=driver.findElement(By.xpath("//a[text()='Computers']")).isDisplayed();
-		//System.out.println(isDisplay);
-		
-		//Xpath with contains(): -> only substring required
-//		driver.findElement(By.xpath("//input[contains(@placeholder,'Sea')")).sendKeys("sagar");
-		
-		//Xpath with start-with(): -> only starting substring required
-		driver.findElement(By.xpath("//input[starts-with(@placeholder,'Sea')")).sendKeys("sagar");
-		
-		
-		
+    public static void main(String[] args) {
+        
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver();
+        
+        // Open the website and maximize the browser window
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        
+        // Examples of using various XPath expressions:
 
-	}
+        // 1. XPath with a single attribute:
+        // Syntax: //tagname[@attribute='value']
+        // driver.findElement(By.xpath("//input[@placeholder='Search store']")).sendKeys("sagar");
 
+        // 2. XPath with multiple attributes:
+        // Syntax: //tagname[@attribute1='value1'][@attribute2='value2']
+        // driver.findElement(By.xpath("//input[@placeholder='Search store'][@id='small-searchterms']")).sendKeys("sagar vishwakarma");
+
+        // 3. XPath with 'and' operator:
+        // Syntax: //tagname[@attribute1='value1' and @attribute2='value2']
+        // driver.findElement(By.xpath("//input[@placeholder='Search store' and @id='small-searchterms']")).sendKeys("sagar vishwakarma");
+
+        // 4. XPath with 'or' operator:
+        // Syntax: //tagname[@attribute1='value1' or @attribute2='value2']
+        // driver.findElement(By.xpath("//input[@placeholder='Search store' or @id='small-searchterms']")).sendKeys("sagar vishwakarma");
+
+        // 5. XPath with text():
+        // Syntax: //tagname[text()='inner text']
+        // Boolean isDisplay = driver.findElement(By.xpath("//h1[text()='Computers']")).isDisplayed();
+        // System.out.println(isDisplay);
+
+        // Alternative example with a link:
+        // Boolean isDisplay = driver.findElement(By.xpath("//a[text()='Computers']")).isDisplayed();
+        // System.out.println(isDisplay);
+
+        // 6. XPath with contains():
+        // Syntax: //tagname[contains(@attribute, 'substring')]
+        // driver.findElement(By.xpath("//input[contains(@placeholder, 'Sea')]")).sendKeys("sagar");
+
+        // 7. XPath with starts-with():
+        // Syntax: //tagname[starts-with(@attribute, 'starting substring')]
+        //driver.findElement(By.xpath("//input[starts-with(@placeholder, 'Sea')]")).sendKeys("sagar");
+        
+        // Close the driver (if needed)
+        // driver.quit();
+    }
 }
